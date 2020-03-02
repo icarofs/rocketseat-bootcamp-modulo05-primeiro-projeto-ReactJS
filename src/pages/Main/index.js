ea7d7ec60bb935bca8559fb8adbdfe71cd82de71
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-03-02 15:17:14 -0300

    stylizing main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>Main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithub />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
